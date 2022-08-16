2cde377357cda1551864aa374b2b77f058b36d7c
Luis Hong <luishong50@gmail.com>
2022-08-15 21:41:45 -0500

    forms of for loop

---
+++ for_loops.rb
@@ -0,0 +1,17 @@
+minerals = ["Nitra","Oil Shale","Gold","Umanite"]
+
+# for mineral in minerals
+#     puts mineral
+# end 
+
+# minerals.each do |m|#the argument between the pipes refers to each element in the array
+#     puts m
+# end
+
+# for index in 0..5#looping from 0 to 5 both inclusive
+#     puts index 
+# end
+
+10.times do |t|
+    puts "hello there"
+end
